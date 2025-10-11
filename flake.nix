75ed5017057b9eb524e5f9bb2e8c0ac5c934232c
TZIKADA <dan0krumkop@gmail.com>
2025-10-11 17:59:11 +0300

    Switch to stable - 25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
 	description = "My laptop flake";
 
 	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 	};
 
 	outputs = { self, nixpkgs }: 
